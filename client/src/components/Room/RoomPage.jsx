c6f9176aeb0a9b63444a343aa6e8d0d81aff59b6
Aniket Gupta <aniketgupta.org@gmail.com>
2024-04-06 19:24:30 +0530

    fix known bugs

--- client/src/components/Room/RoomPage.jsx
+++ client/src/components/Room/RoomPage.jsx
@@ -1,42 +1,46 @@
+import { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import { useSelector } from "react-redux";
 
-
 const RoomPage = () => {
-
     const user = useSelector((state) => state.auth.user);
-// console.log(user);
-
     const { roomId } = useParams();
+    const elementRef = useRef(null);
 
-    console.log(roomId);
+    useEffect(() => {
+        const myMeeting = async () => {
+            const APP_ID = 1252116654;
+            const SERVER_SECRET = '82bbddcff0bcf536cf5a84e3df34ceca';
+            const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(APP_ID, SERVER_SECRET, roomId, user._id, user.fullName);
+            const zc = ZegoUIKitPrebuilt.create(kitToken);
 
-    const myMeeting = async (element) => {
+            zc.joinRoom({
+                container: elementRef.current,
+                sharedLinks: [{
+                    name: "Copy Link",
+                    url: `http://localhost:5173/room/${roomId}`,
+                }],
+                scenario: {
+                    mode: ZegoUIKitPrebuilt.OneONoneCall
+                },
+                showScreenSharingButton: true,
+            });
+        };
 
-        const APP_ID = 1252116654;
-        const SERVER_SECRET = '82bbddcff0bcf536cf5a84e3df34ceca';
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(APP_ID, SERVER_SECRET, roomId, user._id, user.fullName);
-        const zc =  ZegoUIKitPrebuilt.create(kitToken);
+        myMeeting();
 
-        zc.joinRoom({
-            container: element,
-            sharedLinks: [{
-                name: "Copy Link",
-                url: `http://localhost:5173/room/${roomId}`,
-            }],
-            scenario: {
-                mode: ZegoUIKitPrebuilt.OneONoneCall
-            },
-            showScreenSharingButton: true,
-        })
+        // Cleanup function to leave the room when the component unmounts
+        return () => {
+            // Add cleanup code here if necessary
+        };
+    }, [roomId, user]);
 
-    }
     return (
         <div>
-            <div ref={myMeeting} />
+            <div ref={elementRef} />
         </div>
     );
-}
+};
 
 export default RoomPage;
