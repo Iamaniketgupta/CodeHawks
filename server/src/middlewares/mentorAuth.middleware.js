24393f143278abce050b71ff366c00c836933895
Aniket Gupta <aniketgupta.org@gmail.com>
2024-04-07 20:20:06 +0530

    update

--- server/src/middlewares/mentorAuth.middleware.js
+++ server/src/middlewares/mentorAuth.middleware.js
@@ -9,7 +9,7 @@ export const verifyMentorJwt = asyncHandler(async( req , res, next)=>{
         const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer" , "");
     
         if(!token){
-            throw new ApiError(401, "Unauthorized request");
+             res.staus(401).json( {message:`Unauthorized request  ${token}`} );
         }
     
         const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
