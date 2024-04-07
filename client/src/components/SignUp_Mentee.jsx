e047f0b53dd4efcfcf441f5223ee5bdf0a41a12d
Aniket Gupta <aniketgupta.org@gmail.com>
2024-04-07 17:39:40 +0530

    add

--- client/src/components/SignUp_Mentee.jsx
+++ client/src/components/SignUp_Mentee.jsx
@@ -54,7 +54,7 @@ export default function SignUp_Mentee() {
     e.preventDefault();
     try {
       setLoading(true);
-      const response = await axios.post("/api/v1/mentee/signup", formData);
+      const response = await axios.post("https://mentorhub-s8mg.onrender.com/api/v1/mentee/signup", formData);
     //   console.log(response.data);
       toast.success("Sign up successful!");
       setLoading(false);
