1d178890e29e8e25aa4196cb277eb96b63552962
tj <tirtheshjaintj@gmail.com>
2024-04-07 12:57:34 +0530

    HomePage Done

--- client/src/components/menteeDashboard/SubBox.jsx
+++ client/src/components/menteeDashboard/SubBox.jsx
@@ -5,15 +5,7 @@ import {Link} from 'react-router-dom';
 export default function SubBox(props) {
     const [subs,setSubs]=useState([]);
 function getSubs(){
-  
     try {
-      if(props.type=="top"){
-          (async ()=>{
-              const response = await axios.get('/api/v1/subscription/getMenteeSubscriptions');
-              setSubs(response.data.data.slice(0,3));
-              console.log(response.data.data);
-
-            
     if(props.type=="top"){
         (async ()=>{
             const response = await axios.get('/api/v1/subscription/getMenteeSubscriptions');
@@ -29,14 +21,7 @@ function getSubs(){
 
           
           })();
-        }
-        else{
-          (async ()=>{
-               const response = await axios.get('/api/v1/subscription/getMenteeSubscriptions');
-               setSubs(response.data.data);
-               console.log(response.data.data);
-            })();
-        }
+        }    
     } catch (error) {
       console.log(error)
     }
